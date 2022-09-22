14f4bfa7d344e66dc3d3fd88b1744dc4c2580727
Harithaa S <79746981+harithaa12@users.noreply.github.com>
2022-09-23 01:44:19 +0530

    Create AND.sv

---
+++ Verilog programs/AND.sv
@@ -0,0 +1,32 @@
+module andg(A, B, Y);
+  input A,B;
+  output Y;
+  assign Y = A & B;
+endmodule
+
+module andg_tb;
+  reg A,B;
+  wire Y;
+andg andg_tb(.A(A), .B(B),.Y(Y));
+  
+  initial begin 
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    $monitor(A,B,Y);
+    A =1'b0;
+    B= 1'b0;
+    #10
+    A =1'b0;
+    B= 1'b1;
+    #10
+    A =1'b1;
+    B= 1'b0;
+    #10 
+    A =1'b1;
+    B= 1'b1;
+    #10
+    $finish;
+  end
+  always @(Y)
+    $display( "time =%0t \tINPUT VALUES: \t A=%b B =%b \t output value Y =%b",$time,A,B,Y);
+endmodule
